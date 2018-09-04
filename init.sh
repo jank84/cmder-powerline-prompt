7c125c4f199807e26106d33b536520fcca44cd0a
Jk <jan.k@fu-berlin.de>
2018-09-04 15:01:01 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,9 +1,9 @@
+#!/bin/sh
+# type
 # bash <(curl -s https://raw.githubusercontent.com/jank84/cmder-powerline-prompt/master/init.sh)
 
-#!/bin/sh
-echo "Hello, world!"
-#sudo apt-get update && sudo apt-get upgrade
-#sudo apt-get install zsh
-#sudo apt-get install git
-#sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-#git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+echo "init sh my zsh WSL"
+sudo apt-get update -y && sudo apt-get upgrade -y
+sudo apt-get install zsh git curl -y
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
